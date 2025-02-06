8ded23eded90ed94c2fac22089e6e45f052de6fc
tchfer <tchfernando@gmail.com>
2025-02-06 01:01:21 -0300

    Ajustar documentacao dos endpoints

--- src/main/java/br/com/fiap/ez/fastfood/adapters/in/controller/UserController.java
+++ src/main/java/br/com/fiap/ez/fastfood/adapters/in/controller/UserController.java
@@ -4,6 +4,11 @@ import br.com.fiap.ez.fastfood.application.dto.UserRequestDTO;
 import br.com.fiap.ez.fastfood.application.dto.UserResponseDTO;
 import br.com.fiap.ez.fastfood.application.usecases.UserUseCase;
 import br.com.fiap.ez.fastfood.domain.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,19 +16,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
+@Tag(name = "User Operations", description = "Operations related to users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserUseCase userUseCase;
 
-    @PostMapping
-    public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO userRequestDTO) {
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "User created"),
+    @ApiResponse(responseCode = "400", description = "Invalid input data") })
+    @PostMapping(path = "/create-new", produces = "application/json")
+    public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         UserResponseDTO userResponseDTO = userUseCase.registerUser(userRequestDTO);
         return ResponseEntity.ok(userResponseDTO);
     }
 
-    @GetMapping("/{email}")
+    @Operation(summary = "Find user by Email")
+    @GetMapping(path = "/find-by-email/{email}", produces = "application/json")
     public ResponseEntity<User> findUserByEmail(@PathVariable String email) {
         Optional<User> user = userUseCase.findByEmail(email);
         return user.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
